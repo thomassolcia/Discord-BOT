bf65cdcf9983f94924d0b54ac2e02d41b96c9e34
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-05-12 02:38:09 -0300

    Update tempo.js

--- commands/informativos/tempo.js
+++ commands/informativos/tempo.js
@@ -2,19 +2,28 @@ const weather = require('weather-js')
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-    weather.find({search: args.join(' '), degreeType: 'C'}, function(result){
-    var current = result[0].current;
-    var location = result[0].location;
-    const tempEmbed = new Discord.MessageEmbed()
-    .setDescription(`**${current.skytext}**`)
-    .setAuthor(`Clima atual na região de: ${current.observationpoint}`)
-    .setThumbnail(current.imageUrl)
-    .setColor('BLUE')
-    .addField(`Fuso horário`, `UTC${location.timezone}`, true)
-    .addField(`Temperatura`, `${current.temperature} Cº`, true)
-    .addField(`Sensação`, `${current.feelslike} Cº`, true)
-    .addField(`Vento`, `${current.winddisplay}`, true)
-    .addField(`Umidade`, `${current.humidity}%`, true)
-    message.channel.send(tempEmbed)
+
+    weather.find({search: args.join(' '), degreeType: 'C'}, function(err, result) {
+        if (err) message.channel.send(err);
+
+        if(result[0] != undefined){
+            var current = result[0].current;
+            var location = result[0].location;
+            const tempEmbed = new Discord.MessageEmbed()
+            .setDescription(`**${current.skytext}**`)
+            .setAuthor(`Clima atual na região de: ${current.observationpoint}`)
+            .setThumbnail(current.imageUrl)
+            .setColor('BLUE')
+            .addField(`Fuso horário`, `UTC${location.timezone}`, true)
+            .addField(`Temperatura`, `${current.temperature} Cº`, true)
+            .addField(`Sensação`, `${current.feelslike} Cº`, true)
+            .addField(`Vento`, `${current.winddisplay}`, true)
+            .addField(`Umidade`, `${current.humidity}%`, true)
+            message.channel.send(tempEmbed)
+        }else{
+            message.reply('Esta localização não está em meu alcance! Me desculpe.')
+        }
     })
 }
+    
+    
