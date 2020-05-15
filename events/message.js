258c85df05848494ae6a19f05d2d4f315100d93a
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-05-15 00:59:11 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -23,7 +23,14 @@ module.exports = (client, message) => {
   const musicBot = ["play", 'np', 'skip', 'pause', 'next', 'stop', 'queue', 'volume', 'resume'];
   if (musicBot.some(word1 => message.content.includes(word1))) return;
   
-  if ( message.channel.name === 'bot-spam'){
+  if (message.channel.name === 'bot-spam'){
+    try {
+      exports.run(client, message, args);
+    }
+    catch(err) {
+      message.reply('este comando não existe ou o formato está incorreto. Digite `=comandos` para mais informações!');
+    }
+  }else if (message.member.hasPermission("BAN_MEMBERS")){
     try {
       exports.run(client, message, args);
     }
@@ -32,5 +39,5 @@ module.exports = (client, message) => {
     }
   }else{
     message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.")
-  }
+  }  
 };
