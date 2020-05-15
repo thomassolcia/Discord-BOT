24c9b4436f5c713a58164d127eff09f8d5237c86
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-05-15 06:13:13 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,12 +4,12 @@ module.exports = async client => {
     let myGuild = client.guilds.cache.get('364926866487902208');
     let memberCount = myGuild.memberCount;
     let memberCountChannel = myGuild.channels.cache.get('704815480967266385');
-    memberCountChannel.setName('👥 | Usuários: ' + memberCount);
+    memberCountChannel.setName('👥│Usuários: ' + memberCount);
     console.log('Usúarios no servidor: ' + memberCount);
     setInterval(function () {
         let pingAPIChannel = myGuild.channels.cache.get('705183603650199572');
         let test = myGuild.channels.cache.get('705140258647572510');
-        pingAPIChannel.setName(`📶 | Ping API: ${Math.floor(client.ws.ping)}ms`);
+        pingAPIChannel.setName(`📶│Ping API: ${Math.floor(client.ws.ping)}ms`);
 
         let totalSeconds = (client.uptime / 1000);
         let days = Math.floor(totalSeconds / 86400);
@@ -19,10 +19,12 @@ module.exports = async client => {
         let seconds = totalSeconds % 60;
 
         if (days < 1){
-            test.setName(`🕒 | Uptime: ${hours}h:${minutes}m`);
-        }else if (days >= 1)
-            test.setName(`🕒 | Uptime: ${days} dias`);
+            test.setName(`🕒│Uptime: ${hours}h:${minutes}m:${seconds}s`);
+        }else if (days = 1){
+            test.setName(`🕒│Uptime: ${days} dia`);
+        }else if (days > 1)
+            test.setName(`🕒│Uptime: ${days} dias`);
 
-    }, 60000)
+    }, 5000)
     client.user.setActivity("=comandos", { type: "PLAYING" });
 }
