f5a61ff0706213d1622725fffbbd04a9d738e5a1
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-06-15 04:58:50 -0300

    Create grole.js

---
+++ commands/staff/grole.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+
+    if (message.mentions.users.size === 0) {
+        return message.reply("Por favor, mencione um usuário!");
+    }
+
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`Você não tem permissão para usar este comando`);
+    
+if(!args[1]) return message.channel.send(`Você precisa inserir um cargo para ser adicionado.`);
+
+let cargo = args[1]
+let motivo = args[2]
+
+let role = message.guild.roles.cache.find(r => r.name === cargo);
+let member = message.mentions.members.first();
+
+const embed = new Discord.MessageEmbed()
+                            .setDescription(`**Cargo adicionado**`)
+                            .setColor("RED")
+                            .addField("Usuário", `${member}`, true)
+                            .addField("Staff", message.author.username, true)
+                            .setThumbnail(member.displayAvatarURL)
+                            .addField("Obs.", `${motivo}`, true)
+                            .setFooter(`${message.author.tag}`)
+                            .setTimestamp()
+
+                            message.channel.send(embed);
+
+member.roles.add(role).catch(console.error);
+}
