7899eccf5bd8d751dbbc34361769778006a674b1
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-06-22 05:58:26 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,10 +1,6 @@
-module.exports = (client, message) => {
+const Discord = require('discord.js');
 
-  //const swearWords = ["mongo", 'mongoloide', 'mongolóide', 'mongólico', 'monogolico'];
-  //if (swearWords.some(word => message.content.includes(word))) {
-  //message.reply("Cuidado! A `m-word` não está habilitada neste servidor. Não insista!");
-  //message.delete().catch(e => {});
-  //}
+module.exports = (client, message) => {
 
   if (message.author.bot) return;
 
@@ -17,18 +13,23 @@ module.exports = (client, message) => {
   if(message.channel.type === 'dm') {
 		message.reply('Você só pode executar este comando num servidor.')
   } 
-  
-  //const comandos = ['ban', 'kick', 'limpar', 'listban', 'poll', 'moeda', 'ppt', 'roll', 'sorte', 'avatar', 'bot', 'clima', 'comandos', 'ping', 'server', 'user', 'users', 'year', 'cat', 'dog', 'goat', 'raccon', '8ball', 'ascii', 'bigtext', 'biscoito', 'calc', 'dick', 'frase', 'hex', 'igor', 'imgur', 'inverter', 'lembrete', 'lenny', 'morse', 'nick', 'sugerir', 'tapa', "play", 'np', 'skip', 'pause', 'next', 'stop', 'queue', 'volume', 'resume'];
-
+ 
   if (message.channel.name === 'bot-spam' || message.member.hasPermission("BAN_MEMBERS")){
     try {
       exports.run(client, message, args);
     }
     catch(err) {
-      message.reply(err + 'este comando não existe ou o formato está incorreto. Digite `=comandos` para mais informações!');
+        const embed = new Discord.MessageEmbed() 
+        .setTitle(`Este comando não existe ou o formato está incorreto.`)
+        .setDescription("Digite `=ajuda` para mais informações!")
+        .addField('Erro:',`\`${err}\``)
+        .setColor("ORANGE")
+        .setTimestamp()
+        .setFooter(message.author.tag)
+        message.channel.send(embed)
     }
   }else{
     message.delete();
-    message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.")
+    message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.").then(msg => msg.delete({ timeout: 5000, reason: 'Feito!' }))
   }  
 };
