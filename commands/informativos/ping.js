ee8b270f8a6b8e325bc7955cc69b3ecbcfadb2d9
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-07-17 22:36:31 -0300

    Update ping.js

--- commands/informativos/ping.js
+++ commands/informativos/ping.js
@@ -1,10 +1,3 @@
-exports.run = async (client, message) => {
-	const ms = await message.channel.send("Ping?");
-	const clientms = ms.createdTimestamp - message.createdTimestamp;
-	ms.edit('Pong!\n\nSua latência com o bot: ' + clientms + 'ms');
-	message.channel.send(`A Latencia da API é ${Math.floor(client.ws.ping)}ms`);
-}
-
 const Discord = require('discord.js');
 
 exports.run = async (client, message) => {
