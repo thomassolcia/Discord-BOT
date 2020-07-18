2977ccffccb7bb2655d9fa32ab10e4916b35878d
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-07-17 22:36:22 -0300

    Update dog.js

--- commands/animais/dog.js
+++ commands/animais/dog.js
@@ -3,15 +3,25 @@ const superagent = require("superagent");
 
 exports.run = async (bot, message, args) => {
 
-  let {body} = await superagent
-  .get(`https://random.dog/woof.json`);
+  try{
+    let {body} = await superagent
+    .get(`https://random.dog/woof.json`);
 
-   let dogembed = new Discord.MessageEmbed()
-   .setColor("PURPLE")
-   .setTitle("Doguinhos :dog:")
-   .setImage(body.url)
-   .setTimestamp()
-   .setFooter(message.author.tag);
-   message.channel.send(dogembed);
-
+    let dogembed = new Discord.MessageEmbed()
+    .setColor("PURPLE")
+    .setTitle("Doguinhos :dog:")
+    .setImage(body.url)
+    .setTimestamp()
+    .setFooter(message.author.tag);
+    message.channel.send(dogembed);
+  }catch(err){
+    const embed = new Discord.MessageEmbed() 
+    .setTitle(`Este comando não está disponível no momento!`)
+    .setDescription("Contate alguém da staff para mais informações.")
+    .addField('Erro:',`\`${err}\``)
+    .setColor("PURPLE")
+    .setTimestamp()
+    .setFooter(message.author.tag)
+    message.channel.send(embed)
+  }
 }
