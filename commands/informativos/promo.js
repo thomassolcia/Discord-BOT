e55ee524a00f63826ad9eab585882a2379d05e31
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-07-21 02:41:46 -0300

    Update promo.js

--- commands/informativos/promo.js
+++ commands/informativos/promo.js
@@ -1,25 +1,21 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
-let link = args[3]
 let name = args[0]
 let until = args[1]
 let schedule = args[2]
-      
-    if(!args[0]){
-        message.reply(`Você precisa inserir o nome do jogo, data e hora final da promoção e o link.`)
-    }
+let link = args[3]
 
-    if(!args[1]){
-        message.reply(`Você precisa inserir a data e hora final da promoção e o link.`)
-    }
-
-    if(!args[2]){
-        message.reply(`Você precisa inserir a hora que a promoção acaba e o link.`)
-    }
-
-    if(!args[3]){
-        message.reply(`Você precisa inserir o link da promoção.`)
+    if(!args[0] || !args[1] || !args[2] || !args[3]){
+        const embed = new Discord.MessageEmbed() 
+        .setTitle(`Este comando está incorreto.`)
+        .setDescription("Digite `=ajuda` para mais informações ou siga os passos abaixo!")
+        .addField('Tente assim:',`\`=promo {nome_Jogo} {data_Final} {horario_Final} {link}\``)
+        .setColor("BLUE")
+        .setTimestamp()
+        .setFooter(message.author.tag)
+        message.channel.send(embed)
+        return;
     }
 
     const offEmbed = new Discord.MessageEmbed()
@@ -31,5 +27,5 @@ let schedule = args[2]
     .addField(`Link`, `${link}`)
     .setFooter(message.author.tag)
     message.delete();
-    client.channels.cache.get(`707809716205518868`).send(offEmbed)
+    client.channels.cache.get(`718011857771823114`).send(offEmbed)
 }
