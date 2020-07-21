8043def5abae2ce390ad45de13b8e782c4842ca6
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-07-21 02:41:55 -0300

    Delete guildMemberAdd.js

--- events/guildMemberAdd.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client, member) => {
-    let myGuild = client.guilds.cache.get('364926866487902208');
-    let memberCount = myGuild.memberCount;
-    console.log('Usúarios no servidor: ' + memberCount);
-    let memberCountChannel = myGuild.channels.cache.get('704815480967266385');
-    memberCountChannel.setName('👥 | Usuários: ' + memberCount);
-
-    member.guild.channels.cache.get('694285200582115418').send(`<@${user.id}> chegou na reabilitação. Apoiem ele!`)
-}
