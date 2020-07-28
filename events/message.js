ccff7c5113eb0a428b6befa9269860f15f972e0d
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-07-28 20:58:06 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -11,25 +11,26 @@ module.exports = (client, message) => {
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
   if(message.channel.type === 'dm') {
-		message.reply('Você só pode executar este comando num servidor.')
-  } 
- 
-  if (message.channel.name === 'bot-spam' || message.member.hasPermission('MANAGE_MESSAGES', { checkAdmin: true, checkOwner: true })){
-    try {
-      exports.run(client, message, args);
-    }
-    catch(err) {
-        const embed = new Discord.MessageEmbed() 
-        .setTitle(`Este comando não existe ou o formato está incorreto.`)
-        .setDescription("Digite `=ajuda` para mais informações!")
-        .addField('Erro:',`\`${err}\``)
-        .setColor("ORANGE")
-        .setTimestamp()
-        .setFooter(message.author.tag)
-        message.channel.send(embed)
-    }
+    message.reply('Você só pode executar este comando num servidor.')
+    return
   }else{
-    message.delete();
-    message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.").then(msg => msg.delete({ timeout: 5000, reason: 'Feito!' }))
-  }  
+    if (message.channel.name === 'bot-spam' || message.member.hasPermission("KICK_MEMBERS")){
+      try {
+        exports.run(client, message, args);
+      }
+      catch(err) {
+          const embed = new Discord.MessageEmbed() 
+          .setTitle(`Este comando não existe ou o formato está incorreto.`)
+          .setDescription("Digite `=ajuda` para mais informações!")
+          .addField('Erro:',`\`${err}\``)
+          .setColor("ORANGE")
+          .setTimestamp()
+          .setFooter(message.author.tag)
+          message.channel.send(embed)
+      }
+    }else{
+      message.delete();
+      message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.").then(msg => msg.delete({ timeout: 5000, reason: 'Feito!' }))
+    }  
+  }
 };
