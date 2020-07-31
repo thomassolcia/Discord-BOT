a5c4ef6539985ea4d68100dcc3d000d5605ef6c0
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-07-30 21:58:03 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -10,27 +10,27 @@ module.exports = (client, message) => {
 
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-  if(message.channel.type === 'dm') {
+  if (message.channel.type === 'dm') {
     message.reply('Você só pode executar este comando num servidor.')
     return
-  }else{
-    if (message.channel.name === 'bot-spam' || message.member.hasPermission("KICK_MEMBERS")){
+  } else {
+    if (message.channel.name === 'bot-spam' || message.member.hasPermission("KICK_MEMBERS")) {
       try {
         exports.run(client, message, args);
       }
-      catch(err) {
-          const embed = new Discord.MessageEmbed() 
+      catch (err) {
+        const embed = new Discord.MessageEmbed()
           .setTitle(`Este comando não existe ou o formato está incorreto.`)
           .setDescription("Digite `=ajuda` para mais informações!")
-          .addField('Erro:',`\`${err}\``)
+          .addField('Erro:', `\`${err}\``)
           .setColor("ORANGE")
           .setTimestamp()
           .setFooter(message.author.tag)
-          message.channel.send(embed)
+        message.channel.send(embed)
       }
-    }else{
+    } else {
       message.delete();
       message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.").then(msg => msg.delete({ timeout: 5000, reason: 'Feito!' }))
-    }  
+    }
   }
 };
