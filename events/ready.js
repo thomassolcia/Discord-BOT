8ae603bf5abcf197289450d90a290b672b8ad9b3
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-08-06 02:47:46 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,8 @@
 module.exports = async client => {
-    console.log('\n     [STATUS]')
-    console.log('\nBOT PRONTO PARA USO!');
+    console.log('\n     [Pronto para utilização]      \n')
     let myGuild = client.guilds.cache.get('364926866487902208');
     let memberCount = myGuild.memberCount;
     let memberCountChannel = myGuild.channels.cache.get('704815480967266385');
     memberCountChannel.setName('👥│Usuários: ' + memberCount);
-    console.log('Usúarios no servidor: ' + memberCount);
-
     client.user.setActivity("=ajuda", { type: "PLAYING" })
 }
