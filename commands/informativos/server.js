5b8210a5bf7d569c84072dfd828690e24616767e
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-08-12 05:44:31 -0300

    Update server.js

--- commands/informativos/server.js
+++ commands/informativos/server.js
@@ -11,27 +11,27 @@ exports.run = async (client, message, args) => {
         return days + (days == 1 ? " dia" : " dias") + " atrás";
     };
     const embed = new Discord.MessageEmbed()
-    .setColor('BLUE')
-    .setTimestamp(new Date())
-    .setTitle("Informações do Servidor: " + message.guild.name)
-    .setThumbnail('https://cdn.discordapp.com/avatars/704392967074349087/2a2ae76986efdcdf549d9bd0dcedeafc.png')
-    .addField('ID', message.guild.id)
-    .addField('Criado por', message.guild.owner.user.username + '#' + message.guild.owner.user.discriminator)
-    .addField('Região', message.guild.region)
-    .addField('Usuários', memberCount)
-    .addField('Qtd. Cargos', message.guild.roles.cache.size)
-    .addField('Qtd. Canais', message.guild.channels.cache.size)
-    .addField('Nível de verificação', message.guild.verificationLevel + ' | ' + " Todos são bem-vindos em nosso servidor!")
-    .addField('Criação', formatDate('DD/MM/YYYY, às HH:mm:ss', date) + ' | ' + checkDays(date))
-    .setTimestamp()
-    .setFooter(message.author.tag);
+        .setColor('BLUE')
+        .setTimestamp(new Date())
+        .setTitle("Informações do Servidor: " + message.guild.name)
+        .setThumbnail('https://cdn.discordapp.com/avatars/704392967074349087/2a2ae76986efdcdf549d9bd0dcedeafc.png')
+        .addField('ID', message.guild.id)
+        .addField('Criado por', message.guild.owner.user.username + '#' + message.guild.owner.user.discriminator)
+        .addField('Região', message.guild.region)
+        .addField('Usuários', memberCount)
+        .addField('Qtd. Cargos', message.guild.roles.cache.size)
+        .addField('Qtd. Canais', message.guild.channels.cache.size)
+        .addField('Nível de verificação', message.guild.verificationLevel + ' | ' + " Todos são bem-vindos em nosso servidor!")
+        .addField('Criação', formatDate('DD/MM/YYYY, às HH:mm:ss', date) + ' | ' + checkDays(date))
+        .setTimestamp()
+        .setFooter(message.author.tag);
     message.channel.send(embed);
-    
-        function formatDate(template, date) {
-          var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':')
-          date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4)
-          return date.toISOString().split(/[-:.TZ]/).reduce(function (template, item, i) {
-              return template.split(specs[i]).join(item)
-          }, template)
-        }
+
+    function formatDate(template, date) {
+        var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':')
+        date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4)
+        return date.toISOString().split(/[-:.TZ]/).reduce(function (template, item, i) {
+            return template.split(specs[i]).join(item)
+        }, template)
+    }
 }
