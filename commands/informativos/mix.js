7daed888c3bf07b85628f4e7342405315589abd1
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-08-12 05:44:35 -0300

    Update mix.js

--- commands/informativos/mix.js
+++ commands/informativos/mix.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
-
     function shuffleArray(array) {
-
         for (var i = array.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * i);
             var temp = array[i];
@@ -12,17 +10,12 @@ exports.run = async (client, message, args) => {
         }
         return array;
     }
-
     function splitTeams(names) {
-        return `Time 1: \n\`${names.splice(0, Math.floor(names.length/2))}\`\n\nTime 2: \n\`${names}\``
+        return `Time 1: \n\`${names.splice(0, Math.floor(names.length / 2))}\`\n\nTime 2: \n\`${names}\``
     }
     mix = [args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]]
     var teams = splitTeams(shuffleArray(mix))
-
-    //message.channel.send(teams)
-
     function shuffleArrayMaps(array) {
-    
         for (var i = array.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * i);
             var temp = array[i];
@@ -31,24 +24,20 @@ exports.run = async (client, message, args) => {
         }
         return array;
     }
-
     function splitMaps(names) {
-        return `\n\`${names.splice(0, Math.floor(names.length/7))}\``
+        return `\n\`${names.splice(0, Math.floor(names.length / 7))}\``
     }
     mix = ['Mirage', 'Cobblestone', 'Inferno', 'Vertigo', 'Nuke', 'Overpass', 'Dust II']
     var maps = splitMaps(shuffleArrayMaps(mix))
-
     const embed = new Discord.MessageEmbed()
-      .setColor('BLUE')
-      .setTimestamp(new Date())
-      .setTitle('Vila mix')
-      .addField('------- Times -------',teams)
-      .addField('------- Mapa -------',maps)
-      .setTimestamp()
-      .setFooter(message.author.tag);
-      let msg = await message.channel.send({embed});
-
-	await msg.react('👍');
-	await msg.react('👎');
-
+        .setColor('BLUE')
+        .setTimestamp(new Date())
+        .setTitle('Vila mix')
+        .addField('------- Times -------', teams)
+        .addField('------- Mapa -------', maps)
+        .setTimestamp()
+        .setFooter(message.author.tag);
+    let msg = await message.channel.send({ embed });
+    await msg.react('👍');
+    await msg.react('👎');
 }
