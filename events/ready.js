e4263fc2809e84bc3fdf115ccb3c31e0a3931a99
awoone <thomas.nascimento@edu.unifil.br>
2020-10-28 13:03:02 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,10 @@
-module.exports = async client => {
+const Discord = require('discord.js');
+module.exports = async(client, message) => {
     console.log('\n     [COMANDOS PREPARADOS]      \n')
-    setInterval(function() {
-        client.user.setActivity("-ajuda", { type: "PLAYING" })
-    }, 60000);
+    setInterval(() => {
+        var quotes = ['-ajuda', '🎃 Spookytober 🎃', '👻👻']
+        client.user.setActivity(
+            quotes[Math.floor(quotes.length * Math.random())], { type: 'PLAYING' }
+        );
+    }, 10000);
 }
