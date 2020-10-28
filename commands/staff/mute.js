39b0b813087c4f7d849b733e5e1b9773104cffc5
awoone <thomas.nascimento@edu.unifil.br>
2020-10-28 13:02:45 -0300

    Update mute.js

--- commands/staff/mute.js
+++ commands/staff/mute.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 exports.run = async(client, message, args) => {
-    message.delete();
     if (!message.member.hasPermission("BAN_MEMBERS")) {
         return message.channel.send(`${message.author}, você não possui permissão para executar esse comando.`).then(msg => msg.delete(8000))
     } else {
@@ -13,6 +12,7 @@ exports.run = async(client, message, args) => {
                 .setThumbnail(user.displayAvatarURL())
                 .setDescription(`O usuário ${member} foi mutado por tempo indefinido!`)
                 .addField("Staff", message.author.username, true)
+                .addField("Motivo", args[2], true)
             message.channel.send(embed)
         } else {
             var segundos = args[1];
@@ -24,6 +24,7 @@ exports.run = async(client, message, args) => {
                 .setThumbnail(user.displayAvatarURL())
                 .setDescription(`O usuário ${member} foi mutado por ${segundos} segundos!`)
                 .addField("Staff", message.author.username, true)
+                .addField("Motivo", args[2], true)
             message.channel.send(embed).then(msg => msg.delete({ timeout: `${segundos}` * 1000 }))
             setTimeout(() => { member.voice.setMute(false) }, `${segundos}` * 1000);
         }
