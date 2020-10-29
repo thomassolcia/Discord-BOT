2fb2f0873a35bdaf406178d02ee0b3340c87807f
awoone <thomas.nascimento@edu.unifil.br>
2020-10-29 19:06:03 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,28 +5,51 @@ module.exports = (client, message) => {
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const exports = client.commands.get(command);
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    //if (message.content.indexOf(client.config.prefix) !== 0) return;
     if (message.channel.type === 'dm') {
         message.reply('Você só pode executar este comando num servidor.')
         return
     } else {
-        if (message.channel.name === 'bot-spam' || message.member.hasPermission("KICK_MEMBERS")) {
-            try {
-                exports.run(client, message, args);
-            } catch (err) {
-                var d = new Date();
-                var n = `${d.getHours()}:${d.getMinutes()}`;
-                console.log(`Ocorreu um erro: ${err}\nComando utilizado: ${message.content}\nHorário: ${n}\n`)
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`Este comando não existe ou o formato está incorreto.`)
-                    .setDescription("Digite `-ajuda` para mais informações!")
-                    .addField('Erro:', `\`${err}\``)
-                    .setColor("ORANGE")
-                message.channel.send(embed)
+        if (message.content.startsWith(`!`) || message.content.startsWith(`-`) || message.content.startsWith(`=`) || message.content.startsWith(`.`)) {
+            if (message.content.startsWith(`=`) && message.channel.name === 'bot-spam') {
+                try {
+                    exports.run(client, message, args);
+                } catch (err) {
+                    var d = new Date();
+                    var n = `${d.getHours()}:${d.getMinutes()}`;
+                    console.log(`Ocorreu um erro: ${err}\nComando utilizado: ${message.content}\nHorário: ${n}\n`)
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`Este comando não existe ou o formato está incorreto.`)
+                        .setDescription("Digite `=ajuda` para mais informações!")
+                        .addField('Erro:', `\`${err}\``)
+                        .setColor("BLACK")
+                    message.channel.send(embed)
+                }
+            } else if (message.content.startsWith(`!`) && message.channel.name === 'bot-spam') {
+                return;
+            } else if (message.content.startsWith(`-`) && message.channel.name === 'bot-spam') {
+                return;
+            } else if (message.content.startsWith(`.`) && message.channel.name === 'bot-spam') {
+                return;
+            } else {
+                message.delete();
+                message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.").then(msg => msg.delete({ timeout: 5000, reason: 'Feito!' }))
             }
-        } else {
-            message.delete();
-            message.reply("os comandos agora só estão habilitados no canal <#694285200582115418> para evitarmos qualquer tipo de spam e termos uma melhor organização dentro do servidor.").then(msg => msg.delete({ timeout: 5000, reason: 'Feito!' }))
+        } else if ((message.content.startsWith(`https://`) || message.content.startsWith(`www.`) || message.content.endsWith(`.com`) || message.content.endsWith(`.br`)) && message.member.hasPermission("KICK_MEMBERS")) {
+            return;
+        } else if ((message.content.startsWith(`https://`) || message.content.startsWith(`www.`) || message.content.endsWith(`.com`) || message.content.endsWith(`.br`)) && !message.member.hasPermission("KICK_MEMBERS")) {
+            if (message.channel.name === 'links') {
+                return;
+            } else {
+                msgLink = message.content;
+                message.delete({ timeout: 5000 });
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(`LINK REDIRECIONADO`)
+                    .setDescription(`**User:** ${message.author.username}\n**Canal:** ${message.channel.name} -> links\n**Link:** <${msgLink}>`)
+                    .setFooter(`Links serão sempre redirecionados para este canal para evitar um futuro spam`)
+                    .setColor("BLACK")
+                client.channels.cache.get(`771483573219754015`).send(embed)
+            }
         }
     }
 };
