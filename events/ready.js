ef65f516e0ad7365ffc568400df935cab76a06d8
awoone <thomas.nascimento@edu.unifil.br>
2020-10-29 19:06:05 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = async(client, message) => {
     console.log('\n     [COMANDOS PREPARADOS]      \n')
     setInterval(() => {
-        var quotes = ['-ajuda', '🎃 Spookytober 🎃', '👻👻']
+        var quotes = ['=ajuda', '🎃 Spookytober 🎃', '👻👻']
         client.user.setActivity(
             quotes[Math.floor(quotes.length * Math.random())], { type: 'PLAYING' }
         );
