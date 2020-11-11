defddf4d40aec2895ac7e9bcf8611279a6575f2f
awoone <thomas.nascimento@edu.unifil.br>
2020-11-11 15:58:22 -0300

    Create rm.js

---
+++ commands/uteis/rm.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+exports.run = async (client, message, args, level, id) => {
+    var id = db.add(`guild_${message.guild.id}_rmID_${id}`, 1)
+    console.log(id)
+    var time = args[0];
+    var reminder = args.splice(1).join(' ');
+
+    if (!time) return message.channel.send('Não posso te lembrar se você não definir um tempo...').then(message.delete({ timeout: 5000 }));;
+    if (!reminder) return message.channel.send('Você esqueceu de inserir uma mensagem!').then(message.delete({ timeout: 5000 }));
+
+    time = await time.toString();
+    if (time.indexOf('s') !== -1) {
+        var timesec = await time.replace(/s.*/, '');
+        var timems = await timesec * 1000;
+    } else if (time.indexOf('m') !== -1) {
+        var timemin = await time.replace(/m.*/, '');
+        timems = await timemin * 60 * 1000;
+    } else if (time.indexOf('h') !== -1) {
+        var timehour = await time.replace(/h.*/, '');
+        timems = await timehour * 60 * 60 * 1000;
+    } else if (time.indexOf('d') !== -1) {
+        var timeday = await time.replace(/d.*/, '');
+        timems = await timeday * 60 * 60 * 24 * 1000;
+    } else {
+        return message.reply('O tempo deve ser númerico [s/m/h/d]');
+    }
+    const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle(`✉️ Lembrete #${id}`)
+        .setDescription(`Lembrarei você de \`${reminder}\` daqui \`${time}\` em seu chat privado!`)
+    message.channel.send(embed)
+    setTimeout(function () {
+        const embed = new Discord.MessageEmbed()
+            .setColor("GREEN")
+            .setTitle(`✉️ Lembrete #${id}`)
+            .setDescription(`${time} atrás você pediu para te lembrar de \`${reminder}\``)
+        message.author.send(embed)
+    }, parseInt(timems));
+};
