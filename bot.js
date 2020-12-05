ea3343db8a5bd2b60baa026e613944e900235cf7
awoone <thomas.nascimento@edu.unifil.br>
2020-12-05 02:07:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,31 +20,40 @@ client.on("ready", () => {
 
 client.on("ready", () => {
     setInterval(() => {
+        brasil = moment().tz('America/Sao_Paulo').format('H:mm')
         var d = new Date();
         var n = d.getDay() - 1
-        if (n == '1') {
+        if (brasil == '0:01' && n == '1') {
+            vtMonthly.set(`stats.${"364926866487902208"}`, {});
+            mdbMonthly.set(`stats.${"364926866487902208"}`, {});
             vtMonthly.set(`stats.${"713158092061278278"}`, {});
             mdbMonthly.set(`stats.${"713158092061278278"}`, {});
         } else {
             return;
         }
-    }, 86400000);
+    }, 60000);
     setInterval(() => {
+        brasil = moment().tz('America/Sao_Paulo').format('H:mm')
         var d = new Date();
         var n = d.getDay() - 1
-        if (n == '7' || n == '14' || n == '21') {
+        if ((brasil == '0:01' && n == '8') || (brasil == '0:01' && n == '15') || (brasil == '0:01' && n == '22')) {
+            vtWeekly.set(`stats.${"364926866487902208"}`, {});
+            mdbWeekly.set(`stats.${"364926866487902208"}`, {});
             vtWeekly.set(`stats.${"713158092061278278"}`, {});
             mdbWeekly.set(`stats.${"713158092061278278"}`, {});
         } else {
             return;
         }
-    }, 86400000);
+    }, 60000);
     setInterval(() => {
         brasil = moment().tz('America/Sao_Paulo').format('H:mm')
-        if (brasil == '23:59' || brasil == '00:00') {
+        if (brasil == '0:01') {
+            vtDaily.set(`stats.${"364926866487902208"}`, {});
+            mdbDaily.set(`stats.${"364926866487902208"}`, {});
             vtDaily.set(`stats.${"713158092061278278"}`, {});
             mdbDaily.set(`stats.${"713158092061278278"}`, {});
         } else {
+            console.log(brasil)
             return;
         }
     }, 60000);
